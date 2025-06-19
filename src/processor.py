9bcfe4182d7b24ee2f7082ca505fd28351663bc3
Vivek Sivakumar <vivek.a.sivakumar@gmail.com>
2025-06-18 23:04:01 -0400

    pass full lists of what the column names should be

--- src/processor.py
+++ src/processor.py
@@ -68,20 +68,19 @@ class FantasyDataProcessor:
 
         return name
     
-    def parse_awards(self, awards: str) -> List[str]:
+    def parse_awards(self, awards: str) -> int:
         """
-        Parses the awards column and returns a list of awards.
+        Parses the awards column and returns the number of awards.
         """
-        # TODO: fix
         if pd.isna(awards):
-            return []
-        return awards.split(',')
+            return 0
+        return len(awards.split(','))
     
     def combine_year_data(self,
                           file_pattern: str,
+                          column_names: List[str],
                           select_columns: List[str],
                           transformations: Dict[str, Callable],
-                          rename_columns: Dict[str, str],
                           output_file_name: str) -> pd.DataFrame:
         """
         Combines data from multiple years into a single dataframe.
@@ -96,8 +95,11 @@ class FantasyDataProcessor:
         dfs = []
         for file in files:
             df = pd.read_csv(file)
-            df = df[df['Tm'] != 'League Average']
-            df['Year'] = int(file.split('_')[0])
+            assert len(column_names) == len(df.columns), "New column names and dataframe columns must have the same length"
+            df.columns = column_names 
+
+            df = df[df['team'] != 'League Average']
+            df['year'] = int(file.split('_')[0])
             df = df.select(columns=select_columns)
 
             for column, func in transformations.items():
@@ -106,7 +108,6 @@ class FantasyDataProcessor:
                 else:
                     logger.warning(f"Column '{column}' not found in dataframe, skipping transformation.")
             
-            df = df.rename(columns=rename_columns)
             dfs.append(df)
         
         combined_df = pd.concat(dfs, ignore_index=True)
@@ -124,16 +125,50 @@ class FantasyDataProcessor:
         Returns:
             None (saves data to silver layer)
         """
-        select_columns = ['Player', 'Tm', 'FantPos', 'Age', 'G', 'GS', 'FantPt', 'PPR','Year']
-        transformations = {'Player': self.standardize_name}
-        rename_columns = {'Player': 'name', 'Tm': 'team', 'FantPos': 'position', 'Age': 'age', 'G': 'games', 'GS': 'games_started', 'FantPt': 'standard_points', 'PPR': 'ppr_points', 'Year': 'year'}
+        column_names = [
+            'rank',
+            'player',
+            'team',
+            'position',
+            'age',
+            'games',
+            'games_started',
+            'pass_completions',
+            'pass_attempts',
+            'pass_yards',
+            'pass_touchdowns',
+            'pass_interceptions',
+            'rush_attempts',
+            'rush_yards',
+            'rush_yards_per_attempt',
+            'rush_touchdowns',
+            'rec_targets',
+            'rec_receptions',
+            'rec_yards',
+            'rec_yards_per_reception',
+            'rec_touchdowns',
+            'fumbles',
+            'fumbles_lost',
+            'total_touchdowns',
+            'two_point_conversions',
+            'two_point_conversion_passes',
+            'standard_fantasy_points',
+            'ppr_fantasy_points',
+            'dk_fantasy_points',
+            'fd_fantasy_points',
+            'value_over_replacement',
+            'position_rank',
+            'overall_rank'
+        ]
+        select_columns = ['player', 'team', 'position', 'age', 'games', 'games_started', 'standard_fantasy_points', 'ppr_fantasy_points', 'value_over_replacement'] 
+        transformations = {'player': self.standardize_name}
         output_file_name = "player_fantasy_stats.csv"
 
         self.combine_year_data(
             file_pattern="*_player_fantasy_stats.csv",
+            column_names=column_names,
             select_columns=select_columns,
             transformations=transformations,
-            rename_columns=rename_columns,
             output_file_name=output_file_name
         )
     
@@ -142,38 +177,40 @@ class FantasyDataProcessor:
         Reads in all years of receiving stats from the bronze layer, cleans the data and merges them all into one dataframe.
         Saves the dataframe to the silver layer.
         """
-        select_columns = ['Player', 'Tm', 'Age', 'Pos', 'Year', 'Tgt', 'Rec', 'Yds', 'TD', '1D', 'YBC', 'YBC/R', 'YAC', 'YAC/R', 'ADOT', 'BrkTkl', 'Rec/Br', 'Drop%', 'Int', 'Rat', 'Awards']
-        transformations = {'Player': self.standardize_name, 'Awards': self.parse_awards}
-        rename_columns = {
-            'Player': 'name',
-            'Tm': 'team',
-            'Age': 'age',
-            'Pos': 'position',
-            'Year': 'year',
-            'Tgt': 'targets',
-            'Rec': 'receptions',
-            'Yds': 'receiving_yards',
-            'TD': 'receiving_touchdowns',
-            '1D': 'receiving_first_downs',
-            'YBC': 'catch_air_yards',
-            'YBC/R': 'catch_air_yards_per_reception',
-            'YAC': 'yards_after_catch',
-            'YAC/R': 'yards_after_catch_per_reception',
-            'ADOT': 'average_depth_of_target',
-            'BrkTkl': 'receiving_broken_tackles',
-            'Rec/Br': 'receptions_per_broken_tackle',
-            'Drop%': 'drop_percentage',
-            'Int': 'interceptions_when_targeted',
-            'Rat': 'passer_rating_when_targeted',
-            'Awards': 'awards'
-        }
+        column_names = [
+            'rank',
+            'player',
+            'age',
+            'team',
+            'position',
+            'games',
+            'games_started',
+            'rec_receptions',
+            'rec_targets',
+            'rec_yards',
+            'rec_first_downs',
+            'rec_yards_before_catch',
+            'rec_yards_before_catch_per_reception',
+            'rec_yards_after_catch',
+            'rec_yards_after_catch_per_reception',
+            'rec_average_depth_of_target',
+            'rec_broken_tackles',
+            'rec_receptions_per_broken_tackle',
+            'rec_drops',
+            'rec_drop_percentage',
+            'rec_interceptions_when_targeted',
+            'rec_passer_rating_when_targeted',
+            'rec_awards'
+        ]
+        select_columns = ['player', 'team', 'position', 'age' 'rec_receptions', 'rec_targets', 'rec_yards', 'rec_first_downs', 'rec_yards_before_catch', 'rec_yards_before_catch_per_reception', 'rec_yards_after_catch', 'rec_yards_after_catch_per_reception', 'rec_average_depth_of_target', 'rec_broken_tackles', 'rec_receptions_per_broken_tackle', 'rec_drops', 'rec_drop_percentage', 'rec_interceptions_when_targeted', 'rec_passer_rating_when_targeted', 'rec_awards']
+        transformations = {'player': self.standardize_name, 'rec_awards': self.parse_awards}
         output_file_name = "player_receiving_stats.csv"
         
         self.combine_year_data(
             file_pattern="*_player_receiving_stats.csv",
+            column_names=column_names,
             select_columns=select_columns,
             transformations=transformations,
-            rename_columns=rename_columns,
             output_file_name=output_file_name
         )
     
@@ -182,32 +219,34 @@ class FantasyDataProcessor:
         Reads in all years of rushing stats from the bronze layer, cleans the data and merges them all into one dataframe.
         Saves the dataframe to the silver layer.
         """
-        select_columns = ['Player', 'Tm', 'Age', 'Pos', 'Year', 'Att', 'Yds', '1D', 'YBC', 'YBC/ATT', 'YAC', 'YAC/Att', 'BrkTkl', 'Att/Br', 'Awards']
-        transformations = {'Player': self.standardize_name, 'Awards': self.parse_awards}
-        rename_columns = {
-            'Player': 'name',
-            'Tm': 'team',
-            'Age': 'age',
-            'Pos': 'position',
-            'Year': 'year',
-            'Att': 'attempts',
-            'Yds': 'rushing_yards',
-            '1D': 'rushing_first_downs',
-            'YBC': 'rush_yards_before_contact',
-            'YBC/ATT': 'rush_yards_before_contact_per_attempt',
-            'YAC': 'rush_yards_after_contact',
-            'YAC/Att': 'rush_yards_after_contact_per_attempt',
-            'BrkTkl': 'rush_broken_tackles',
-            'Att/Br': 'rush_attempts_per_broken_tackle',
-            'Awards': 'awards'
-        }
+        column_names = [
+            'rank',
+            'player',
+            'age',
+            'team',
+            'position',
+            'games',
+            'games_started',
+            'rush_attempts',
+            'rush_yards',
+            'rush_first_downs',
+            'rush_yards_before_contact',
+            'rush_yards_before_contact_per_attempt',
+            'rush_yards_after_contact',
+            'rush_yards_after_contact_per_attempt',
+            'rush_broken_tackles',
+            'rush_attempts_per_broken_tackle',
+            'rush_awards'
+        ]
+        select_columns = ['player', 'team', 'position', 'age' 'rush_attempts', 'rush_yards', 'rush_first_downs', 'rush_yards_before_contact', 'rush_yards_before_contact_per_attempt', 'rush_yards_after_contact', 'rush_yards_after_contact_per_attempt', 'rush_broken_tackles', 'rush_attempts_per_broken_tackle', 'rush_awards']
+        transformations = {'player': self.standardize_name, 'rush_awards': self.parse_awards}
         output_file_name = "player_rushing_stats.csv"
         
         self.combine_year_data(
             file_pattern="*_player_rushing_stats.csv",
+            column_names=column_names,
             select_columns=select_columns,
             transformations=transformations,
-            rename_columns=rename_columns,
             output_file_name=output_file_name
         )
     
@@ -216,43 +255,59 @@ class FantasyDataProcessor:
         Reads in all years of passing stats from the bronze layer, cleans the data and merges them all into one dataframe.
         Saves the dataframe to the silver layer.
         """
-        select_columns = ['Player', 'Tm', 'Age', 'Pos', 'Year', 'Cmp', 'Att', 'IAY', 'IAY/PA', 'CAY', 'CAY/Cmp', 'CAY/PA', 'YAC', 'YAC/Cmp', 'ThAway', 'Drop%', 'BadTh%', 'OnTgt%', 'PktTime', 'Bltz', 'Hrry', 'Hits', 'Prss%', 'Scrm', 'Yds/Scrm', 'Awards']
-        transformations = {'Player': self.standardize_name}
-        rename_columns = {
-            'Player': 'name',
-            'Tm': 'team',
-            'Age': 'age',
-            'Pos': 'position',
-            'Year': 'year',
-            'Cmp': 'completions',
-            'Att': 'attempts',
-            'IAY': 'incomplete_air_yards',
-            'IAY/PA': 'incomplete_air_yards_per_attempt',
-            'CAY': 'completed_air_yards',
-            'CAY/Cmp': 'completed_air_yards_per_completion',
-            'CAY/PA': 'completed_air_yards_per_attempt',
-            'YAC': 'yards_after_catch',
-            'YAC/Cmp': 'yards_after_catch_per_completion',
-            'ThAway': 'passes_thrown_away',
-            'Drop%': 'drop_percentage',
-            'BadTh%': 'bad_throw_percentage',
-            'OnTgt%': 'on_target_percentage',
-            'PktTime': 'pocket_time',
-            'Bltz': 'blitz_count',
-            'Hrry': 'hurry_count',
-            'Hits': 'hit_count',
-            'Prss%': 'pressure_percentage',
-            'Scrm': 'scramble_count',
-            'Yds/Scrm': 'yards_per_scramble',
-            'Awards': 'awards'
-        }
+        column_names = [
+            'rank',
+            'player',
+            'age',
+            'team',
+            'position',
+            'games',
+            'games_started',
+            'pass_completions',
+            'pass_attempts',
+            'pass_incomplete_air_yards',
+            'pass_incomplete_air_yards_per_attempt',
+            'pass_completed_air_yards',
+            'pass_completed_air_yards_per_completion',
+            'pass_completed_air_yards_per_attempt',
+            'pass_yards_after_catch',
+            'pass_yards_after_catch_per_completion',
+            'pass_passes_batted',
+            'pass_passes_thrown_away',
+            'pass_spikes',
+            'pass_drops',
+            'pass_drop_percentage',
+            'pass_bad_throws',
+            'pass_bad_throw_percentage'
+            'pass_on_target_throws',
+            'pass_on_target_percentage',
+            'pass_pocket_time',
+            'pass_blitzes',
+            'pass_hurries',
+            'pass_hits',
+            'pass_pressures',
+            'pass_pressure_percentage',
+            'pass_scrambles',
+            'pass_yards_per_scramble',
+            'rpo_plays',
+            'rpo_yards',
+            'rpo_pass_attemps',
+            'rpo_pass_yards',
+            'rpo_rush_attempts',
+            'rpo_rush_yards',
+            'play_action_pass_attempts',
+            'play_action_pass_yards',
+            'awards'
+        ]
+        select_columns = ['player', 'team', 'position', 'age', 'pass_incomplete_air_yards', 'pass_completed_air_yards', 'pass_yards_after_catch', 'pass_passes_thrown_away', 'pass_drops', 'pass_bad_throws', 'pass_on_target_throws', 'pass_pocket_time', 'pass_blitzes', 'pass_hurries', 'pass_hits', 'pass_pressures', 'awards']
+        transformations = {'player': self.standardize_name, 'awards': self.parse_awards}
         output_file_name = "player_passing_stats.csv"
 
         self.combine_year_data(
             file_pattern="*_player_passing_stats.csv",
+            column_names=column_names,
             select_columns=select_columns,
             transformations=transformations,
-            rename_columns=rename_columns,
             output_file_name=output_file_name
         )
 
@@ -265,16 +320,42 @@ class FantasyDataProcessor:
         Returns:
             None (saves data to silver layer)
         """
-        # TODO: fix
-        select_columns = ['Tm', 'G', 'Year', 'Yds', 'Ply', 'Y/P', 'TO', 'FL',  'PassYds', 'PassTD', 'PassInt', 'RushYds', 'RushTD', 'Rec', 'RecYds', 'RecTD', 'Fmb', 'Year']
-        rename_columns = {'Tm': 'team', 'PassYds': 'passing_yards', 'PassTD': 'passing_touchdowns', 'PassInt': 'passing_interceptions', 'RushYds': 'rushing_yards', 'RushTD': 'rushing_touchdowns', 'Rec': 'receptions', 'RecYds': 'receiving_yards', 'RecTD': 'receiving_touchdowns', 'Fmb': 'fumbles', 'Year': 'year'}
+        column_names = [
+            'rank',
+            'team',
+            'games',
+            'plays',
+            'team_yards_per_play',
+            'team_turnovers',
+            'team_fumbles_lost',
+            'team_first_downs',
+            'team_pass_completions',
+            'team_pass_attempts',
+            'team_pass_yards',
+            'team_pass_touchdowns',
+            'team_pass_interceptions',
+            'team_pass_net_yards_per_attempt',
+            'team_pass_first_downs',
+            'team_rush_attempts',
+            'team_rush_yards',
+            'team_rush_touchdowns',
+            'team_rush_yards_per_attempt',
+            'team_rush_first_downs',
+            'team_penalties',
+            'team_penalty_yards',
+            'team_penalty_first_downs',
+            'team_scoring_percent',
+            'team_turnover_percent',
+            'team_expected_points'
+        ]
+        select_columns = ['team', 'plays', 'team_yards_per_play', 'team_turnovers', 'team_first_downs', 'team_pass_completions', 'team_pass_attempts', 'team_pass_yards', 'team_pass_touchdowns', 'team_pass_interceptions', 'team_pass_net_yards_per_attempt', 'team_pass_first_downs', 'team_rush_attempts', 'team_rush_yards', 'team_rush_touchdowns', 'team_rush_yards_per_attempt', 'team_rush_first_downs', 'team_scoring_percent', 'team_turnover_percent', 'team_expected_points']
         output_file_name = "team_offense.csv"
 
         self.combine_year_data(
             file_pattern="*_team_offense.csv",
+            column_names=column_names,
             select_columns=select_columns,
             transformations={},
-            rename_columns=rename_columns,
             output_file_name=output_file_name
         )
 
